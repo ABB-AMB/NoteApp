fe10d16eccaa807e8eda847b6b4daf9ed7e0bdee
ABB-AMB <a422502864@gmail.com>
2026-02-02 20:37:01 +0200

    Create Appbar

---
+++ lib/views/NotesView.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/CustomAppBar.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(children: [SizedBox(height: 50), CustomAppBar()]),
+      ),
+    );
+  }
+}
