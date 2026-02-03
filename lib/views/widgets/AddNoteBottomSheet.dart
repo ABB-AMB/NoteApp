2b5fca701ebcbe36e90bede84a41198e64f392df
ABB-AMB <a422502864@gmail.com>
2026-02-03 23:27:47 +0200

    adding the Add Button and finishing the BottomSheet UI

--- lib/views/widgets/AddNoteBottomSheet.dart
+++ lib/views/widgets/AddNoteBottomSheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/CustomBotton.dart';
 import 'package:notes_app/views/widgets/CustomTextField.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,13 +9,18 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          CustomTextField(hint: "Titel"),
-          SizedBox(height: 24),
-          CustomTextField(hint: "Content", maxLines: 5),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32),
+            CustomTextField(hint: "Titel"),
+            SizedBox(height: 16),
+            CustomTextField(hint: "Content", maxLines: 5),
+            SizedBox(height: 40),
+            CustomBotton(),
+            SizedBox(height: 35),
+          ],
+        ),
       ),
     );
   }
