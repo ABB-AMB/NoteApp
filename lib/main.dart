68e490f0d2579459aedab923994226bb7a275525
ABB-AMB <a422502864@gmail.com>
2026-02-08 13:52:29 +0200

    provideAddNoteCubit to the app and add it to addNoteBottomSheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_ce_flutter/hive_ce_flutter.dart';
 import 'package:notes_app/Constants.dart';
+import 'package:notes_app/Cubits/AddNoteCubit/add_note_cubit_cubit.dart';
 import 'package:notes_app/models/NoteModel.dart';
 import 'package:notes_app/views/NotesView.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
-   Hive.registerAdapter(NoteModelAdapter());
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 
@@ -16,10 +18,13 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark, fontFamily: "Poppins"),
-      home: NotesView(),
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => AddNoteCubitCubit())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: "Poppins"),
+        home: NotesView(),
+      ),
     );
   }
 }
--- lib/views/widgets/AddNoteBottomSheet.dart
+++ lib/views/widgets/AddNoteBottomSheet.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/Cubits/AddNoteCubit/add_note_cubit_cubit.dart';
 import 'package:notes_app/views/widgets/NoteForm.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,8 +11,24 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: SingleChildScrollView(child: NoteForm()),
+      child: SingleChildScrollView(
+        child: BlocConsumer<AddNoteCubitCubit, AddNoteCubitState>(
+          listener: (context, state) {
+            if (state is AddNoteCubitSuccess) {
+              Navigator.of(context).pop();
+            }
+            if (state is AddNoteCubitFailuer) {
+              print(state.errMessage);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteCubitLoadding ? true : false,
+              child: NoteForm(),
+            );
+          },
+        ),
+      ),
     );
   }
 }
-
